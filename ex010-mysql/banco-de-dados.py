23d2144f02b4576f66ee5c6b1668196ae28f524e
NandoJunioDev <frnndmaz_cf@outlook.com>
2025-08-19 07:26:11 -0300

    teste de banco de dados

--- ex010-mysql/banco-de-dados.py
+++ ex010-mysql/banco-de-dados.py
@@ -1,16 +1,36 @@
-import sqlite3 as banco_de_dados
+import sqlite3
 
-conexao = banco_de_dados.connect("empresa.db")
-cursor = conexao.cursor()
-cursor.execute( '''
-CREATE TABLE IF NOT EXISTS usuarios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE
-)
-'''
-)
-print("tabela criada com sucesso")
-conexao.commit()
-cursor.close()
-conexao.close()
+def main():
+    try:
+     #aqui o with gerencia tudo, como fecha o arquivo e comittaaaar é boa pratica
+        with sqlite3.connect('banco-de-dados.db') as conexao:
+            cursor = conexao.cursor()
+
+            #1 Criar tabela usando cursor.execute
+            cursor.execute(''' CREATE TABLE IF NOT EXISTS usuarios (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            nome TEXT NOT NULL,
+                            email  TEXT NOT NULL UNIQUE ) 
+                            ''')
+
+            #2 vamos inserir os dados agora na tabela criada
+            lista_usuarios = [
+                ("mamador","arrombado@gmail.com"),("sentarindo","peteratinha@gmail.com")
+            ]
+            for nome, email in lista_usuarios:
+                cursor.execute(" INSERT OR IGNORE  INTO usuarios (nome, email) VALUES (?,?)" , ( nome, email) )
+            print("dados inseridos com sucesso ")
+            #3 CONSULTAR OS DADOS
+            cursor.execute("""SELECT id,nome,email FROM usuarios""")
+            print("\n esses sao os usuarios: ")
+            for usuario in cursor.fetchall():
+                print(f"Id:{usuario[0]} nome:{usuario[1]} email:{usuario[2]}")
+    except sqlite3.Error as erro:
+            print(erro)
+
+
+
+
+if __name__ == '__main__': # 4 chamada para funçao main
+        main()
+

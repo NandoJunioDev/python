075a85eb6f60a0e1e863e70e248c73001a92d018
NandoJunioDev <Frnndmaz_cf@outlook.com>
2025-08-21 16:09:57 -0300

    cadad

--- Gui/Integrando-gui-banco-de-dados/ex001-gui-com-banco-de-dados/Config-DB/Config-Postgree.py
+++ Gui/Integrando-gui-banco-de-dados/ex001-gui-com-banco-de-dados/Config-DB/Config-Postgree.py
@@ -1,26 +1,44 @@
 import psycopg2
 from psycopg2 import Error
-
-DB_USER = "postgres"
-DB_PASSWORD = "1234"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB = "Agenda"
+import os
 
 
-    def conectar(self):
+
+DB_USER = os.getenv("")
+DB_PASSWORD = os.getenv("")
+DB_HOST = os.getenv("")
+DB_PORT = os.getenv("")
+DB = os.getenv("")
+
+
+def conectar():
 
         try:
-            connection = None
-            connection = psycopg2.connect(user=DB_USER, host=DB_HOST, port=DB_PORT, password=DB_PASSWORD, database=DB)
-
+            with psycopg2.connect(user = DB_USER,password = DB_PASSWORD,host=DB_HOST,port=DB_PORT,database=DB) as conn:
+                 return conn
+                 
             return connection
 
         except Error  as e:
             print(f"erro  ao conectar  ao banco de dados: {e}" )
-        finally:
-            if connection is not None:
-                connection.close()
-                print("conexao feita com sucesso")
+            return None
+        
 
+if __name__ == "__main__":
+     
+     conn = conectar()
 
+     if conn:
+          print("conexao sucessida")
+          try:
+               with conn.cursor() as cursor:
+                    cursor.execute("SELECT version();")
+                    versao = cursor.fetchone()
+                    print(f"versao do postgree {versao}")
+          except Error  as e:
+               print(f"dadadd {e}")        
+          finally:
+               if conn:
+                    conn.close()
+                    print("conexao fechada")     
+    

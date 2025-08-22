cf8d09b4eebe0e1fd825370c53b0d44398838ec8
NandoJunioDev <frnndmaz_cf@outlook.com>
2025-08-21 21:37:18 -0300

    vamos la

--- Gui/Integrando-gui-banco-de-dados/ex001-gui-com-banco-de-dados/Config-DB/Config-Postgree.py
+++ Gui/Integrando-gui-banco-de-dados/ex001-gui-com-banco-de-dados/Config-DB/Config-Postgree.py
@@ -1,44 +1,46 @@
 import psycopg2
 from psycopg2 import Error
+from dotenv import load_dotenv
 import os
 
 
-
-DB_USER = os.getenv("")
-DB_PASSWORD = os.getenv("")
-DB_HOST = os.getenv("")
-DB_PORT = os.getenv("")
-DB = os.getenv("")
+load_dotenv()
+DB_USER = "postgres"
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = "agenda.cw1aac6s65zn.us-east-1.rds.amazonaws.com"
+DB_PORT ="5432"
+DB_NAME ="agenda"
 
 
 def conectar():
-
+        conn = None
         try:
-            with psycopg2.connect(user = DB_USER,password = DB_PASSWORD,host=DB_HOST,port=DB_PORT,database=DB) as conn:
-                 return conn
-                 
-            return connection
+            conn =  psycopg2.connect(user=DB_USER,password=DB_PASSWORD,host=DB_HOST,port=DB_PORT,database=DB_NAME)
 
+            print("conexao feita com sucesso!")
+
+            return conn
         except Error  as e:
             print(f"erro  ao conectar  ao banco de dados: {e}" )
             return None
         
 
 if __name__ == "__main__":
-     
+
      conn = conectar()
 
      if conn:
-          print("conexao sucessida")
-          try:
-               with conn.cursor() as cursor:
-                    cursor.execute("SELECT version();")
-                    versao = cursor.fetchone()
-                    print(f"versao do postgree {versao}")
-          except Error  as e:
-               print(f"dadadd {e}")        
-          finally:
-               if conn:
-                    conn.close()
-                    print("conexao fechada")     
-    
+        try:
+              with conn as conexao:
+                  with conexao.cursor() as cursor:
+                      cursor.execute("SELECT version()")
+                      versao = cursor.fetchone()
+                      print(versao)
+
+        except Error as e:
+            print(f"erro  {e} ")
+
+        finally:
+            if conn:
+                conn.close()
+                print("conexao fechada")
---
+++ Gui/Integrando-gui-banco-de-dados/ex001-gui-com-banco-de-dados/app/CRUD_CONTATO.py
@@ -0,0 +1,2 @@
+from psycopg2 import Error
+from Config_DBcdgitr.Config_Postgree import conectar
